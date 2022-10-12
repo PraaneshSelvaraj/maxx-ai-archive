87f93d0d6974446f631a779fb4493545ca0ca9a9
PraaneshSelvaraj <praaneshselvaraj2003@gmail.com>
2022-10-12 14:10:03 +0530

    ⚗️Added wit.ai in voice engine

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,33 +1,18 @@
-from time import sleep
-import webbrowser
 from  kivymd.app import MDApp 
 from kivy.lang import Builder
 from kivymd.uix.button import MDFlatButton
 import json
-import os
-import pickle
-import threading
-import socket
-import requests
-from kivy.clock import Clock
-from requests.auth import HTTPBasicAuth
-from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.expansionpanel import MDExpansionPanelOneLine, MDExpansionPanel
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.list import OneLineIconListItem, IconRightWidget, OneLineListItem
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.core.window import Window
-from kivymd.uix.card import MDCard
-from kivymd.uix.templates import StencilWidget
-from kivymd.uix.templates import ScaleWidget
-from kivy.animation import Animation
-from kivy.properties import NumericProperty, StringProperty
-
-Window.keyboard_anim_args = {'d': .2, 't': 'in_out_expo'}
-Window.softinput_mode = "below_target"
-#Window.size = (1080/2, 2134/2)
 
 class MainScreen(Screen):
+    pass
+
+class WitaiContent(MDBoxLayout):
+    pass
+
+class GttsContent(MDBoxLayout):
     pass
 
 class Dashboard(MDApp):
@@ -46,7 +31,15 @@ class Dashboard(MDApp):
             self.root.get_screen('main').ids.darkmode_switch.active=True
         else:
             self.root.get_screen('main').ids.darkmode_switch.active=False
-        self.theme_cls.theme_style=theme
+            self.theme_cls.theme_style=theme
+        witaicontent = WitaiContent()
+        self.root.get_screen('main').ids.card.add_widget(
+            MDExpansionPanel(
+                on_open=self.panel_open,
+                on_close=self.panel_close,
+                content=witaicontent,
+                panel_cls=MDExpansionPanelOneLine(text="Wit.ai"),
+            ))
 
     def change_theme(self):
         if self.root.get_screen('main').ids.darkmode_switch.active==True:
@@ -64,6 +57,20 @@ class Dashboard(MDApp):
         self.root.get_screen('main').ids.nav_drawer.set_state('close')
         self.root.get_screen('main').ids.inner_screen_manager.current=screen
     
+    def panel_open(self, *args):
+        Animation(
+            height=(self.root.ids.box.height + self.root.ids.content.height)
+            - self.theme_cls.standard_increment * 2,
+            d=0.2,
+        ).start(self.root.get_screen('main').ids.box)
+
+    def panel_close(self, *args):
+        Animation(
+            height=(self.root.ids.box.height - self.root.ids.content.height)
+            + self.theme_cls.standard_increment * 2,
+            d=0.2,
+        ).start(self.root.get_screen('main').ids.box)
+
 if __name__ == "__main__":
 	Dashboard().run()
 
